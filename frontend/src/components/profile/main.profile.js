c9ae1658eece03d90daba1e249bb006529c5d300
Jonathan Nakagawa <jnakagawa17@gmail.com>
2019-12-02 09:53:15 -0500

    Added logout button

--- frontend/src/components/profile/main.profile.js
+++ frontend/src/components/profile/main.profile.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import { Typography } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import { Link, useLocation } from 'react-router-dom';
 
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 
@@ -16,14 +18,35 @@ const useStyles = makeStyles({
     icon:{
         color: "rgb(111,183,230)",
         marginBottom: 0,
+        marginTop: 0,
     },
     title: {
         fontWeight: "bold",
         fontSize: 40,
         marginBottom: 20,
         marginTop: 0
+    },
+    logout:{
+        alignSelf: "flex-end",
+        backgroundColor: "rgb(111,183,230)",
+        width: "100px",
+        height: "50px",
+        fontSize: "16px",
+        marginTop: "10px",
+        marginBottom: 0,
+    },
+    link: {
+        textDecoration: 'none',
+        color: 'black',
     }
   });
+
+function logout(e) {
+    e.preventDefault();
+    localStorage.removeItem("key");
+    localStorage.removeItem("username");
+    localStorage.removeItem("email");
+}
 
 export default function Profile() {
     const classes = useStyles();
@@ -33,6 +56,11 @@ export default function Profile() {
             {!localStorage.getItem("key") ? 
                 (<div>Login in Required</div>) : 
                 (<Container className={classes.root}>
+                    <Button className={classes.logout} onClick={(e) => logout(e)} size="small">
+                        <Link className={classes.link} to="/login">
+                            logout
+                        </Link>
+                    </Button>
                     <AccountCircleIcon className={classes.icon} style={{ fontSize: 150 }} />
                     <Typography className={classes.title}>Welcome, {localStorage.getItem("username")}</Typography>
                     <Options/>
